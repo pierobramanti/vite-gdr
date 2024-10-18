47bb03a32715aa42a24cb268de71b94954869f70
Corfaya <lauracorrado0696@gmail.com>
2024-10-18 16:24:24 +0200

    [UPD] router and store js

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { reactive } from "vue";
+
+const store = reactive({
+    url: 'http://127.0.0.1:8000//api',
+    urlCharacters: '/characters',
+    urlItems: '/items'
+})
+
+export { store }
