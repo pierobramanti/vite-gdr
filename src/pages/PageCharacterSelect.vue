8b6cf81d10adb28fb254d7f36e17bd33cd8a38cf
Corfaya <lauracorrado0696@gmail.com>
2024-10-20 17:45:09 +0200

    [UPD] pgs filled into CharacterSelect page

--- src/pages/PageCharacterSelect.vue
+++ src/pages/PageCharacterSelect.vue
@@ -15,6 +15,9 @@ export default {
         getCharacters() {
             axios.get(`${store.url}${store.urlCharacters}`).then((res) => {
                 console.log(res.data.results)
+                this.characters = res.data.results
+            }).catch((error) => {
+                console.error('Errore recupero', error)
             })
         }
     }
@@ -23,12 +26,32 @@ export default {
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-12">Seleziona il tuo personaggio</div>
+            <div class="col-12">
+                <h1>Seleziona il tuo personaggio</h1>
+            </div>
         </div>
-        <div class="row">
-            <div class="col-12 col-md-4">
+        <div class="row gy-3">
+            <div class="col-12 col-md-4" v-for="character in characters" :key="character.id">
                 <div class="card">
-                    <img src="" class="card-img-top">
+                    <div v-if="character.type.image" class="background-images">
+                        <img :src="character.type.image" class="card-img-top p-3" :alt="`${character.type.name} class`">
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-title">
+                            {{ character.name }}
+                        </h3>
+                        <h6>Tipo: {{character.type.name}}</h6>
+                        <p class="card-text" v-if="character.description">{{character.description}}</p>
+                        <p class="card-text" v-else>Nessuna descrizione presente</p>
+                        <h5 class="text-end">Statistiche:</h5>
+                        <ul class="list-unstyled text-end">
+                            <li>Forza: {{ character.strength }} </li>
+                            <li>Difesa: {{ character.defence }} </li>
+                            <li>Destrezza: {{ character.speed }} </li>
+                            <li>Intelligenza: {{ character.intelligence }} </li>
+                            <li>HP max: {{ character.life }} </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
