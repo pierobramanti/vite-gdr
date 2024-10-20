e54d8a0f96538345d1c7e94445a5880cf9ae1109
Corfaya <lauracorrado0696@gmail.com>
2024-10-20 17:30:03 +0200

    [FIX] typing error fixed

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from "vue";
 
 const store = reactive({
-    url: 'http://127.0.0.1:8000//api',
+    url: 'http://127.0.0.1:8000/api',
     urlCharacters: '/characters',
     urlItems: '/items'
 })
